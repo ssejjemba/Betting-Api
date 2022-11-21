fa7092064146451cb8dd0b4153a52674674b3491
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-11-21 23:52:16 +0300

    Add users module for authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { getEnvPath } from './common/helpers/config';
+import { UsersModule } from './users/users.module';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/environment`);
 
 @Module({
-  imports: [ConfigModule.forRoot({ envFilePath, isGlobal: true })],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    ConfigModule.forRoot({ envFilePath, isGlobal: true }),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
