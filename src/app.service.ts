58eba10cf1bf6318ed09fb1064fe93e6dad163e8
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-11-21 23:22:20 +0300

    Add tests for configuration setup

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  @Inject(ConfigService)
+  public config: ConfigService;
+
   getHello(): string {
+    const databaseName: string = this.config.get('POSTGRES_DB_NAME');
+
+    console.log({ databaseName });
     return 'Hello World!';
   }
 }
