404f8081d6d62c4e3e216dcdd14271cefb1687ee
Ssejjemba Dan <ssejjembadan@gmail.com>
2022-11-22 00:01:54 +0300

    Add user repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { getEnvPath } from './common/helpers/config';
 import { UsersModule } from './users/users.module';
+import { User } from './users/user.entity';
 
 const envFilePath: string = getEnvPath(`${__dirname}/common/environment`);
 
@@ -13,7 +14,7 @@ const envFilePath: string = getEnvPath(`${__dirname}/common/environment`);
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'db.sqlite',
-      entities: [],
+      entities: [User],
       synchronize: true,
     }),
     ConfigModule.forRoot({ envFilePath, isGlobal: true }),
